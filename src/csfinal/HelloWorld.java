173d582e7de4952d452d4d220bc601c3c6d8d721
OnezEgo <slavetoego@gmail.com>
2017-05-20 13:49:50 -0700

    commit for my final

---
+++ src/csfinal/HelloWorld.java
@@ -0,0 +1,10 @@
+package csfinal;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Richard.... no its not but thats what im suppose to put");
+
+	}
+
+}
